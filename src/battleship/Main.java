6beb86f40e7813a5ad5148fc66ff83779c60f05d
Yuh1226 <huyga2014@gmail.com>
2026-03-23 18:04:56 +0700

    tạo giao diện 2 bảng mình và địch

--- src/battleship/Main.java
+++ src/battleship/Main.java
@@ -1,5 +1,8 @@
 package battleship;
 
+import javax.swing.*;
+import java.awt.*;
+
 import battleship.model.Board;
 import battleship.view.BoardPanel;
 import battleship.controller.GameController;
@@ -11,19 +14,42 @@ public class Main {
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
-				//Tạo logic
-				Board model = new Board();
-				//Tạo giao diện 
-				BoardPanel view = new BoardPanel();
-				//Kết nối 2 cái bằng controller
-				new GameController(model,view);
+				//1.Model
+				Board p1board = new Board();
+				Board p2board = new Board();
+				
+				//2.View
+				BoardPanel p1view = new BoardPanel();
+				BoardPanel p2view = new BoardPanel();
+				
+				//Cửa sổ
+				JPanel mainPanel = new JPanel(new GridLayout(1, 2, 20, 0));
+                mainPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+
+                JPanel p1Container = new JPanel(new BorderLayout());
+                JLabel p1Label = new JLabel("HẠM ĐỘI CỦA BẠN", SwingConstants.CENTER);
+                p1Label.setFont(new Font("Arial", Font.BOLD, 16));
+                p1Container.add(p1Label, BorderLayout.NORTH);
+                p1Container.add(p1view, BorderLayout.CENTER);
+
+                JPanel p2Container = new JPanel(new BorderLayout());
+                JLabel p2Label = new JLabel("VÙNG BIỂN ĐỊCH (BẮN VÀO ĐÂY)", SwingConstants.CENTER);
+                p2Label.setFont(new Font("Arial", Font.BOLD, 16));
+                p2Container.add(p2Label, BorderLayout.NORTH);
+                p2Container.add(p2view, BorderLayout.CENTER);
+
+                mainPanel.add(p1Container);
+                mainPanel.add(p2Container);
+				
+				//3.Controller
+				new GameController(p1board,p2board,p1view,p2view);
 				
 				//Tạo khung cửa sổ chứa bảng chứ cái
-				JFrame frame = new JFrame("Game bắn tàu - java Swing");
+				JFrame frame = new JFrame("Game Đại Chiến Trên Biển");
 				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-				frame.add(view);
+				frame.add(mainPanel);
 				
-				frame.setSize(600,600);
+				frame.setSize(1000,500);
 				frame.setLocationRelativeTo(null);
 				frame.setVisible(true);
 			}
--- src/battleship/controller/GameController.java
+++ src/battleship/controller/GameController.java
@@ -4,39 +4,33 @@ import battleship.model.Board;
 import battleship.model.Node;
 import battleship.model.Ship;
 import battleship.view.BoardPanel;
+
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Random;
 
 public class GameController {
-	private Board modelBoard;
-	private BoardPanel viewPanel;
+	private Board p1board;
+	private Board p2board;
+	private BoardPanel p1view;
+	private BoardPanel p2view;
 
-	public GameController(Board modelBoard, BoardPanel viewPanel) {
-		this.modelBoard = modelBoard;
-		this.viewPanel = viewPanel;
-		setUpShipP2(modelBoard);
+	public GameController(Board p1board, Board p2board, BoardPanel p1view, BoardPanel p2view) {
+		this.p1board = p1board;
+		this.p2board = p2board;
+		this.p1view = p1view;
+		this.p2view = p2view;
+
+		setUpShip(this.p1board);
+		setUpShip(this.p2board);
+
+		showP1Ships();
+
 		setupMouseClicks();
 	}
-	
-	private void setupMouseClicks() {
-		for (int r = 0; r < Board.size; r++) {
-			for (int c = 0; c < Board.size; c++) {
-				final int row = r;
-				final int col = c;
 
-				viewPanel.getButton(row, col).addActionListener(new ActionListener() {
-					@Override
-					public void actionPerformed(ActionEvent e) {
-						boolean isHit = modelBoard.fireAt(row, col);
-						viewPanel.updateButtonState(row, col, isHit);
-					}
-				});
-			}
-		}
-	}
-	
-	public void setUpShipP2(Board modelBoard) {
+	public void setUpShip(Board modelBoard) {
 		Random rd = new Random();
 		int[] lengths = { 5, 4, 3, 3, 2 };
 
@@ -54,6 +48,38 @@ public class GameController {
 					modelBoard.placeShip(s);
 					placed = true;
 				}
+			}
+		}
+	}
+
+	public void showP1Ships() {
+		Node[][] p1Grid = p1board.getGrid();
+		
+		for (int r = 0; r < p1board.size; r++) {
+			for (int c = 0; c < p1board.size; c++) {
+				if(p1Grid[r][c].getVal() == Node.SHIP) {
+					p1view.getButton(r, c).setBackground(Color.DARK_GRAY);
+					p1view.getButton(r, c).setText("Tàu");
+					p1view.getButton(r, c).setForeground(Color.WHITE);
+				}
+				p1view.getButton(r,c).setEnabled(false);
+			}
+		}
+	}
+
+	private void setupMouseClicks() {
+		for (int r = 0; r < Board.size; r++) {
+			for (int c = 0; c < Board.size; c++) {
+				final int row = r;
+				final int col = c;
+
+				p2view.getButton(row, col).addActionListener(new ActionListener() {
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						boolean isHit = p2board.fireAt(row, col);
+						p2view.updateButtonState(row, col, isHit);
+					}
+				});
 			}
 		}
 	}
