106ce50a0c30d567e7a037b48cff3b7e8ef81147
Yuh1226 <huyga2014@gmail.com>
2026-03-26 18:51:36 +0700

    Sửa xong bot hard, còn phần mvc : đọc file GameWindow.java, bạn sửa nốt đi

--- src/battleship/view/GameWindow.java
+++ src/battleship/view/GameWindow.java
@@ -3,4 +3,10 @@ package battleship.view;
 public class GameWindow {
 	//Chuyển phần giao diện tạm thời bên main sửa sang bên này 
 	// Thêm phần giao diện ở đây
+	
+	//Controller làm việc của Model (Tính toán dữ liệu): Hàm setUpShip() sử dụng Random để dò tìm vị trí và đặt tàu. Theo lý thuyết, việc khởi tạo trạng thái dữ liệu (đặt tàu ở đâu) phải nằm trong Model (Board.java). Controller chỉ nên gọi p1board.autoPlaceShips().
+
+	//Controller làm việc của View (Xử lý giao diện): Hàm showP1Ships() trực tiếp gọi setBackground(Color.DARK_GRAY) và setText("Tàu"). Nguyên tắc là Controller không được phép biết màu sắc hay text hiển thị là gì. Lẽ ra View phải tự có hàm drawShipAt(x, y) và Controller chỉ gọi hàm đó.
+
+	//Chứa thư viện UI: Việc Controller trực tiếp gọi JOptionPane.showMessageDialog khiến nó bị dính chặt với thư viện giao diện Swing.
 }
